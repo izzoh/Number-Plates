80f30ea638738b7a41b5be6be7b2ffa04f5b7412
Isaac Waweru <isaacw@ke.ibm.com>
2017-09-08 19:03:18 +0300

    initial commit

---
+++ src/main/java/com/brave/ventures/live/NumberPlatesApplication.java
@@ -0,0 +1,12 @@
+package com.brave.ventures.live;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberPlatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberPlatesApplication.class, args);
+	}
+}
